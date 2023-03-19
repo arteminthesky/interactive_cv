c31667b99f12afad3b4b9ebf5289c86e0a535555
artemov <akiberq@gmail.com>
2023-03-19 01:02:47 +0100

    Add base BMO widget

--- lib/drawers/right_drawer_page.dart
+++ lib/drawers/right_drawer_page.dart
@@ -3,14 +3,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import '../widgets/fidgets/fidgets.dart';
+
 class RightDrawerPage extends StatelessWidget {
   const RightDrawerPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: Colors.red.withOpacity(0.5),
-      child: SizedBox.expand(),
+    return const SizedBox.expand(
+      child: CustomScrollView(
+        slivers: [
+          SliverSafeArea(
+            bottom: false,
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(15),
+              sliver: SliverToBoxAdapter(
+                child: BmoFidget(),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/widgets/fidgets/bmo_fidget.dart
+++ lib/widgets/fidgets/bmo_fidget.dart
@@ -1,1 +1,54 @@
 part of fidgets;
+
+class BmoFidget extends StatelessWidget {
+  const BmoFidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FidgetPanel(
+      aspectRatio: 2/3,
+      blurred: false,
+      child: CustomPaint(
+        painter: BmoPainter(),
+      ),
+    );
+  }
+}
+
+class BmoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var widgetWidth = size.width;
+
+    var screenSize = Size(widgetWidth, widgetWidth / 1.5);
+
+    _drawBackground(canvas, size);
+    _drawScreen(canvas, screenSize);
+
+    var controlsSize = Size(widgetWidth, size.height - screenSize.height);
+    canvas.translate(0, screenSize.height);
+    _drawControls(canvas, controlsSize);
+  }
+
+  void _drawBackground(Canvas canvas, Size size) {
+    canvas.drawColor(Colors.greenAccent, BlendMode.src);
+  }
+
+  void _drawScreen(Canvas canvas, Size size) {
+    var screenRect = Rect.fromLTWH(0, 0, size.width, size.height);
+    screenRect = screenRect.deflate(10);
+    canvas.drawRRect(
+      RRect.fromRectXY(screenRect, 20, 20),
+      Paint()..color = Colors.green,
+    );
+  }
+
+  void _drawControls(Canvas canvas, Size size) {
+
+  }
+
+  @override
+  bool shouldRepaint(BmoPainter oldDelegate) {
+    return false;
+  }
+}
