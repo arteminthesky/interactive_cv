18a2415dceacc0cd8b71a9bd3ff2687018907e3c
artemov <akiberq@gmail.com>
2023-03-22 02:40:25 +0100

    Move applications into separated directory

--- app_base/lib/app_base.dart
+++
@@ -1,8 +0,0 @@
-library app_base;
-
-export 'package:models/models.dart';
-
-export 'src/application.dart';
-export 'src/option.dart';
-export 'src/overlay_application.dart';
-export 'src/route_application.dart';
--- app_base/lib/src/build_application_mixin.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-import '../app_base.dart';
-
-mixin BuildApplicationMixin on Application {
-  Widget buildApp(BuildContext context);
-}
